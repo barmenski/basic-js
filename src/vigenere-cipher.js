4f0ae059f676c8d102d09467a0af85294f78122a
barmenski <1kiloom@gmail.com>
2021-02-16 20:36:01 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,64 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor(reverse) {
+		this.reverse=reverse;
+	}
+encrypt(m, k){
+	if((m===undefined)||(k===undefined)){
+		throw new Error("Error");
+	}
+	let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let message=m.toUpperCase();
+	let key=k.toUpperCase();
+	var wordMaxlength = message.length;//maxlength равен более длинному из сообщения или ключа
+	var result = '';
+	let c='';	
+	for(let i=0, j=0; i<wordMaxlength; i++, j++){ 			
+		let mi = abc.indexOf( message[ ( (i>=message.length) ? i%message.length : i ) ] );	//определяем индекс буквы из сообщения В АЛФАВИТЕ с учетом подгона длины сообщения - к ключу (если меньше, но обычно нет)
+		let ki = abc.indexOf( key[ ( (j>=key.length) ? j%key.length : j ) ]);	//буква ИЗ КЛЮЧА с учетом подгона длины ключа к сообщению (если короткий, а обычно так и есть)
+
+		ki = ( (typeof mode !== 'undefined' && mode.indexOf('decrypt') !== -1) ?(-ki) :ki );
+		if(abc.indexOf(message[ ( (i>=message.length) ? i%message.length : i ) ])===-1){
+			c=message[ ( (i>=message.length) ? i%message.length : i ) ];
+			j--;
+		} else {
+		c = abc[ ( ( ( abc.length + ( mi + ki ) ) % abc.length ) ) ];	//символ по таблице Виженера.
+		}	
+	
+		result += c;																//Добавить символ к результату.
+	}
+	if(this.reverse===false){
+		return result.split('').reverse().join('');
+	}
+	return result;
+}    
+decrypt(m, k) {
+	if((m===undefined)||(k===undefined)){
+		throw new Error("Error");
+	}
+	let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let message=m.toUpperCase();
+	let key=k.toUpperCase();
+	var wordMaxlength = message.length;//maxlength равен более длинному из сообщения или ключа
+	var result = '';
+	let c='';
+	for(let i=0, j=0; i<wordMaxlength; i++, j++){ 			
+		let mi = abc.indexOf( message[ ( (i>=message.length) ? i%message.length : i ) ] );	//определяем индекс буквы из сообщения В АЛФАВИТЕ с учетом подгона длины сообщения - к ключу (если меньше, но обычно нет)
+		let ki = abc.indexOf( key[ ( (j>=key.length) ? j%key.length : j ) ]);	//буква ИЗ КЛЮЧА с учетом подгона длины ключа к сообщению (если короткий, а обычно так и есть)
+		if(abc.indexOf(message[ ( (i>=message.length) ? i%message.length : i ) ])===-1){
+			c=message[ ( (i>=message.length) ? i%message.length : i ) ];
+			j--;
+		} else {
+		c = abc[ ( ( ( abc.length + ( mi - ki ) ) % abc.length ) ) ];	//символ по таблице Виженера.
+		}			
+		result += c;																//Добавить символ к результату.
+	}
+	if(this.reverse===false){
+		return result.split('').reverse().join('');
+	}
+	return result;
+}
 }
 
 module.exports = VigenereCipheringMachine;
